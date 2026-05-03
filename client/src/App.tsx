9b66e1e0c99ba070ef97c65e233580473184c392
carsnwd <carsnwd@gmail.com>
2026-05-02 20:13:13 -0400

    refactor: migrate from React to Solid.js

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { createSignal } from 'solid-js'
 import beaver from './assets/beaver.svg'
 import type { ApiResponse } from 'shared'
 import './App.css'
@@ -6,7 +6,7 @@ import './App.css'
 const SERVER_URL = import.meta.env.VITE_SERVER_URL || "http://localhost:3000"
 
 function App() {
-  const [data, setData] = useState<ApiResponse | undefined>()
+  const [data, setData] = createSignal<ApiResponse | undefined>()
 
   async function sendRequest() {
     try {
@@ -22,24 +22,31 @@ function App() {
     <>
       <div>
         <a href="https://github.com/stevedylandev/bhvr" target="_blank">
-          <img src={beaver} className="logo" alt="beaver logo" />
+          <img src={beaver} class="logo" alt="beaver logo" />
+        </a>
+        <a href="https://github.com/stevedylandev/bhvr" target="_blank">
+          <img src={beaver} class="logo" alt="beaver logo" />
+        </a>
+        <a href="https://github.com/stevedylandev/bhvr" target="_blank">
+          <img src={beaver} class="logo" alt="beaver logo" />
         </a>
       </div>
-      <h1>bhvr</h1>
-      <h2>Bun + Hono + Vite + React</h2>
+      <h1>bhvrs</h1>
+      <h2>Bun + Hono + Vite + <span style={{ 'text-decoration': 'line-through' }}>React</span> + Solid</h2>
       <p>A typesafe fullstack monorepo</p>
-      <div className="card">
-        <div className='button-container'>
+      <p>Modified from bhvr to use Solid instead of React</p>
+      <div class="card">
+        <div class='button-container'>
           <button onClick={sendRequest}>
             Call API
           </button>
-          <a className='docs-link' target='_blank' href="https://bhvr.dev">Docs</a>
+          <a class='docs-link' target='_blank' href="https://bhvr.dev">Docs</a>
         </div>
         {data && (
-          <pre className='response'>
+          <pre class='response'>
             <code>
-            Message: {data.message} <br />
-            Success: {data.success.toString()}
+              Message: {data()?.message} <br />
+              Success: {data()?.success.toString()}
             </code>
           </pre>
         )}
